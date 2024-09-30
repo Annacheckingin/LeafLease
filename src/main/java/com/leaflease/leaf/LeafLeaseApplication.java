c1803c7d47656ef1d5e0790a9ad227645541fd7d
lizhengguo <1519794332@qq.com>
2024-09-30 14:53:49 +0800

    initial

---
+++ src/main/java/com/leaflease/leaf/LeafLeaseApplication.java
@@ -0,0 +1,13 @@
+package com.leaflease.leaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafLeaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafLeaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leaflease/leaf/LeafLeaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaflease.leaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafLeaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
